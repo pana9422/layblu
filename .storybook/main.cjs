7fbd12b6cbb34cf35670891b32a3b88f13579791
Braider Perez <braiderandreyp@gmail.com>
2023-03-31 16:45:01 -0500

    style: Give format with prettier

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		"@storybook/addon-postcss",
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-postcss',
 	],
-	framework: "@storybook/react",
+	framework: '@storybook/react',
 	core: {
-		builder: "@storybook/builder-vite",
+		builder: '@storybook/builder-vite',
 	},
 	features: {
 		storyStoreV7: true,
 	},
-};
+}
--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,21 +1,34 @@
-import { Button } from "./Button";
+import { Button } from './Button'
+import { Close } from '../Icon'
 
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
-	title: "Button",
+	title: 'Button',
 	component: Button,
-} as ComponentMeta<typeof Button>;
+	args: {
+		children: 'Button',
+	},
+} as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => (
-	<Button {...args}>Default</Button>
-);
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />
 
-export const Default = Template.bind({});
-Default.args = { square: true };
+export const Default = Template.bind({})
+Default.args = { square: true }
 
-export const Outline = Template.bind({});
-Outline.args = { outline: true };
+export const Outline = Template.bind({})
+Outline.args = { outline: true }
 
-export const Text = Template.bind({});
-Text.args = { flat: true };
+export const Text = Template.bind({})
+Text.args = { flat: true }
+
+export const Dense = Template.bind({})
+Dense.args = {
+	dense: true,
+	children: (
+		<>
+			Dense
+			<Close width={20} height={20} fill='white' />
+		</>
+	),
+}

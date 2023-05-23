1e02e1528c1a48212f7ced4447465ef2ff931909
Braider Perez <braiderandreyp@gmail.com>
2023-05-23 16:48:58 -0500

    refactor: Better evaluation for conditional rendering the before

--- src/components/atoms/Item/Item.tsx
+++ src/components/atoms/Item/Item.tsx
@@ -4,13 +4,13 @@ export const Item = ({
 	icon,
 	txt,
 	before,
-	horizontalHoverLine,
+	horizontalHoverLine = false,
 	color = 'indigo-800',
 	beforeColor = 'red-600',
 	borderColor = 'hover:border-indigo-400',
 }: Props) => {
 	const listClass = `border-transparent list-none flex gap-5 py-1.5 px-3 items-center cursor-pointer ${
-		horizontalHoverLine ?? false
+		horizontalHoverLine
 			? `border-b-2 ${borderColor}`
 			: `border-l-2 ${borderColor}`
 	}`
@@ -21,7 +21,7 @@ export const Item = ({
 		<li className={listClass}>
 			{icon}
 			<span className={textClass}>{txt}</span>
-			{before != null && <span className={beforeClass}>{before}</span>}
+			{Boolean(before) && <span className={beforeClass}>{before}</span>}
 		</li>
 	)
 }

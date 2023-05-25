cde209b22a34c20fc41a49e2e7551225e8bfa06b
Braider Perez <braiderandreyp@gmail.com>
2023-05-25 16:05:36 -0500

    refactor: Using the same styling pattern used in another components

--- src/components/atoms/Item/Item.tsx
+++ src/components/atoms/Item/Item.tsx
@@ -9,19 +9,23 @@ export const Item = ({
 	beforeColor = 'red-600',
 	borderColor = 'hover:border-indigo-400',
 }: Props) => {
-	const listClass = `border-transparent list-none flex gap-5 py-1.5 px-3 items-center cursor-pointer ${
-		horizontalHoverLine
-			? `border-b-2 ${borderColor}`
-			: `border-l-2 ${borderColor}`
-	}`
-	const textClass = `text-${color}`
-	const beforeClass = `bg-${beforeColor} py-0.5 px-1.5 rounded-full text-sm text-gray-50 ml-auto`
+	const hoverLine = horizontalHoverLine
+		? `border-b-2 ${borderColor}`
+		: `border-l-2 ${borderColor}`
 
 	return (
-		<li className={listClass}>
+		<li
+			className={`border-transparent list-none flex gap-5 py-1.5 px-3 items-center cursor-pointer ${hoverLine}`}
+		>
 			{icon}
-			<span className={textClass}>{label}</span>
-			{Boolean(before) && <span className={beforeClass}>{before}</span>}
+			<span className={`text-${color}`}>{label}</span>
+			{Boolean(before) && (
+				<span
+					className={`py-0.5 px-1.5 rounded-full text-sm text-gray-50 ml-auto bg-${beforeColor}`}
+				>
+					{before}
+				</span>
+			)}
 		</li>
 	)
 }

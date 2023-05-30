0dc749de8c1ee88407e9e172aa1d1144dadeafc1
Braider Perez <braiderandreyp@gmail.com>
2023-05-30 14:54:11 -0500

    refactor: Remove a repeated conditional class in horizonatalHoverLine

--- src/components/atoms/Item/Item.tsx
+++ src/components/atoms/Item/Item.tsx
@@ -9,13 +9,11 @@ export const Item = ({
 	beforeColor = 'red-600',
 	borderColor = 'hover:border-indigo-400',
 }: Props) => {
-	const hoverLine = horizontalHoverLine
-		? `border-b-2 ${borderColor}`
-		: `border-l-2 ${borderColor}`
+	const hoverLine = horizontalHoverLine ? `border-b-2` : `border-l-2`
 
 	return (
 		<li
-			className={`border-transparent list-none flex gap-5 py-1.5 px-3 items-center cursor-pointer ${hoverLine}`}
+			className={`border-transparent list-none flex gap-5 py-1.5 px-3 items-center cursor-pointer ${hoverLine} ${borderColor}`}
 		>
 			{icon}
 			<span className={`text-${color}`}>{label}</span>
